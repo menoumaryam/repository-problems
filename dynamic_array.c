3b76f81b8689cd66b223e161b8e774d44c75959e
menoumaryam <114919093+menoumaryam@users.noreply.github.com>
2023-03-20 17:48:53 +0000

    Update dynamic_array.c

--- dynamic_array.c
+++ dynamic_array.c
@@ -1,3 +1,5 @@
+//dynamic_array for int:
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -49,4 +51,59 @@ for(int i=0;i<lista.longeur;i++)
     printf("%d;",lista.p[i]);
 }
 return 0;
+}
+//dynamic_array for char:
+#include<stdio.h>
+#include<stdlib.h>
+
+struct list{
+    int longeur;
+    int taille;
+    char *arr;
+};
+typedef struct list list ;
+void append_list(list *A,char x)
+{
+    char n=A->longeur;
+    char y=A->taille;
+          if(n==y)
+          {
+            A->taille=y*2;
+            char*new_arr=(char*)malloc(y*2*sizeof(char));
+            for(int i=0;i<n;i++)
+            {
+               new_arr[i]=(A->arr)[i];
+            }
+            free(A->arr);
+            A->arr=new_arr;
+            (A->longeur)+=1;
+            (A->arr[x+1])=x;
+
+          }
+          else 
+          {
+            (A->longeur)+=1;
+            (A->arr)[A->longeur]=x;
+          }
+
+
+}
+
+int main()
+{
+    struct list A;
+    A.longeur=0;
+    A.taille=1;
+    A.arr=(char*)malloc((A.taille)*sizeof(char));
+    
+for(int i=0;i<100;i++)
+{
+    append_list(&A,i);
+}    
+for(int i=0;i<A.longeur;i++)
+{
+    printf("%c;",A.arr[i]);
+}
+return 0;
+
 }
